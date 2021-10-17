6e5f9491160d4bbfd8bfb8d8f42169a2ca7ec424
gleeko1 <benfattocristian@gmail.com>
2021-10-17 22:01:13 +0200

    feature: add user on navbar
    -fixed username on Welcome {username} on User component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Result } from '../shared/models/user.interface';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  @Input() user!: Result;
   constructor() { }
 
   ngOnInit(): void {
